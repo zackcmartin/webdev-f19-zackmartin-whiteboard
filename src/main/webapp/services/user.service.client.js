95d4b5781dadd86c3c00a8d4d6fe02a023f393d9
Zack Martin <zackmartin@Zacks-MBP.fios-router.home>
2019-09-21 13:16:00 -0400

    Added user admin service client

---
+++ src/main/webapp/services/user.service.client.js
@@ -0,0 +1,14 @@
+function AdminUserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    //this.url = 'https://wbdv-generic-server.herokuapp.com/api/jannunzi/users';
+    var self = this;
+    function createUser(user, callback) {  }
+    function findAllUsers(callback) {  }
+    function findUserById(userId, callback) {  }
+    function updateUser(userId, user, callback) {  }
+    function deleteUser(userId, callback) {  }
+}
